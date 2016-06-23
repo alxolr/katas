d8dde63376692d542b9e3a22e061bbb4c94cc178
Alexandru Olaru <aolaru@gmail.com>
2016-06-23 12:24:19 +0300

    Added the dependency inejector resolver kata

---
+++ test/dependency-injection.test.js
@@ -0,0 +1,63 @@
+/**
+ * Constructor DependencyInjector
+ * @param {Object} - object with dependencies
+ */
+var DI = function(dependency) {
+  this.dependency = dependency;
+};
+
+// Should return new function with resolved dependencies
+DI.prototype.inject = function(func) {
+  let deps = /\(([\w,_\s]+)\)/g.exec(func.toString()),
+    self = this,
+    resolved;
+
+    if (args !== null) {
+      args = args[1];
+      resolved = args.split(',')
+        .map(item => self.dependency[item.trim()]);
+    } else {
+      resolved = [];
+    }
+  return function() {
+    return func(...resolved);
+  };
+};
+
+const assert = require('assert');
+
+describe('DependencyInjector', function() {
+  it('should resolve the injected methods', function() {
+    var deps = {
+      'dep1': function() {
+        return 'this is dep1';
+      },
+      'dep2': function() {
+        return 'this is dep2';
+      },
+      'dep3': function() {
+        return 'this is dep3';
+      },
+      'dep4': function() {
+        return 'this is dep4';
+      }
+    };
+
+    var di = new DI(deps);
+
+    var myFunc = di.inject(function(dep3, dep1, dep2) {
+      return [dep1(), dep2(), dep3()].join(' -> ');
+    });
+    assert.equal(myFunc(), 'this is dep1 -> this is dep2 -> this is dep3');
+
+  });
+
+  it('should resolve the case when no parameters sent', function() {
+    var di = new DI({});
+    var myFunc = di.inject(function() {
+      return "hello world";
+    });
+    assert.equal(myFunc(), 'hello world');
+
+  });
+});
