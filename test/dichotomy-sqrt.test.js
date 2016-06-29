e4a135b0d649c69afa7c39648ab23c28b96b7c9c
Alexandru Olaru <aolaru@gmail.com>
2016-06-29 11:10:09 +0300

    Worked on dichotomy method

---
+++ test/dichotomy-sqrt.test.js
@@ -0,0 +1,26 @@
+const assert = require('assert');
+
+function dichotomySqrt(number) {
+  let lowest = Math.floor(number / 2);
+  while (lowest * lowest > number) {
+    lowest--;
+  }
+
+  let highest = lowest + 1;
+  let middle = (lowest + highest) / 2;
+
+  while ((Math.abs(middle * middle - number)) > 0.0002) {
+    console.log(middle);
+    let pow = middle * middle;
+    if (pow > number) {
+      highest = middle;
+    } else if (pow < number) {
+      lowest = middle;
+    }
+    middle = (lowest + highest) / 2;
+  }
+
+  return middle;
+}
+
+dichotomySqrt(379);
