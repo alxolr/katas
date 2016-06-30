b049e634b1ffcfe547159449158f64f6831ba40b
Alexandru Olaru <aolaru@gmail.com>
2016-06-30 17:17:13 +0300

    Added the robot path tests

---
+++ test/robot-path.test.js
@@ -0,0 +1,70 @@
+const assert = require('assert');
+
+class Robot {
+  constructor(map) {
+    this.map = map;
+  }
+
+  toMatrix() {
+    let side = Math.sqrt(this.map.length);
+    let matrix = [];
+    for (let i = 0; i < side; i++) {
+      matrix.push(this.map.slice(side * i, side + side * i).split(''));
+    }
+
+    return matrix;
+  }
+
+  getStartCoordinates() {
+    let matrix = this.toMatrix();
+
+    for (let i = 0; i < matrix.length; i++) {
+      for (let j = 0; j < matrix.length; j++) {
+        if (matrix[i][j] === 'S') return [i, j];
+      }
+    }
+  }
+
+  getTargetCoordinates() {
+    for (let i = 0; i < matrix.length; i++) {
+      for (let j = 0; j < matrix.length; j++) {
+        if (matrix[i][j] === 'T') return [i, j];
+      }
+    }
+  }
+
+  getMetaMatrix() {
+    let metaMatrix = this.toMatrix(),
+      maxSide = metaMatrix.length;
+
+    const fill = (distance, i, j) => {
+      if (i < maxSide && i >= 0 && j < maxSide && j >= 0 && metaMatrix[i][j] !== undefined) {
+        if ("S.T".indexOf(metaMatrix[i][j]) !== -1) {
+          metaMatrix[i][j] = distance;
+          fill(distance + 1, i, j + 1);
+          fill(distance + 1, i + 1, j);
+          fill(distance + 1, i, j - 1);
+          fill(distance + 1, i - 1, j);
+        } else {
+          if (metaMatrix[i][j] > distance) {
+            metaMatrix[i][j] = distance;
+
+            fill(distance + 1, i, j + 1);
+            fill(distance + 1, i + 1, j);
+            fill(distance + 1, i, j - 1);
+            fill(distance + 1, i - 1, j);
+          }
+        }
+      } else {
+        return;
+      }
+    };
+
+    fill(0, ...this.getStartCoordinates());
+
+    return metaMatrix;
+  }
+}
+
+
+console.log(new Robot("S#..#...T").getMetaMatrix());
