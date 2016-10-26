0666565910298647bbd6afae3725b3af15304f61
Alexandru Olaru <aolaru@pentalog.fr>
2016-10-26 10:07:15 +0300

    Cleaned up the the solved katas

--- test/deep-object-compare.test.js
+++ test/deep-object-compare.test.js
@@ -1,60 +1,61 @@
-function deepCompare(left, right) {
-  if (left === right) return true;
-  if (typeof left !== 'object' || typeof right !== 'object') return false;
-  if (Object.keys(left).length !== Object.keys(right).length) return false;
-  var keys = Object.keys(left);
+/* global describe it */
+function deepCompare (left, right) {
+  if (left === right) return true
+  if (typeof left !== 'object' || typeof right !== 'object') return false
+  if (Object.keys(left).length !== Object.keys(right).length) return false
+  var keys = Object.keys(left)
 
-  return keys.every(function(key) {
-    return deepCompare(left[key], right[key]);
-  });
+  return keys.every(function (key) {
+    return deepCompare(left[key], right[key])
+  })
 }
 
-const assert = require('assert');
+const assert = require('assert')
 
-describe('deepCompare', function() {
-  it('should pass for simple values', function() {
-    assert.equal(deepCompare(1, 1), true);
-  });
+describe('deepCompare', function () {
+  it('should pass for simple values', function () {
+    assert.equal(deepCompare(1, 1), true)
+  })
 
-  it('should return true given the obj1, obj2 paire', function() {
+  it('should return true given the obj1, obj2 paire', function () {
     let obj1 = {
-        name: 'John'
-      },
-      obj2 = {
-        name: 'John'
-      };
-    assert.equal(deepCompare(obj1, obj2), true);
-  });
+      name: 'John'
+    }
+    let obj2 = {
+      name: 'John'
+    }
+    assert.equal(deepCompare(obj1, obj2), true)
+  })
 
-  it('should return false given null and undefined', function() {
-    assert.equal(deepCompare(null, undefined), false);
-  });
+  it('should return false given null and undefined', function () {
+    assert.equal(deepCompare(null, undefined), false)
+  })
 
-  it('should detect when a property is missing', function() {
+  it('should detect when a property is missing', function () {
     assert.equal(deepCompare({
       name: 'John',
       surname: 'Smith'
     }, {
       name: 'John'
-    }), false);
-  });
+    }), false)
+  })
 
-  it('should detect when a property is missing', function() {
+  it('should detect when a property is missing', function () {
     assert.equal(deepCompare({
       name: 'John'
     }, {
       name: 'John',
       surname: 'Smith'
-    }), false);
-  });
+    }), false)
+  })
 
-  it('should compare arrays', function() {
+  it('should compare arrays', function () {
     assert.equal(deepCompare([1, 2, null, undefined, {
       name: 'Joe'
-        }], [1, 2, null, undefined]), false);
-  });
+    }], [1, 2, null, undefined]), false)
+  })
 
-  it('should compare the array order', function() {
-    assert.equal(deepCompare([1, 2, 3], [1, 3, 2]), false);
-  });
-});
+  it('should compare the array order', function () {
+    assert.equal(deepCompare([1, 2, 3], [1, 3, 2]), false)
+  })
+})
--- test/dependency-injection.test.js
+++ test/dependency-injection.test.js
@@ -1,63 +1,62 @@
+/* global describe it */
 /**
  * Constructor DependencyInjector
  * @param {Object} - object with dependencies
  */
-var DI = function(dependency) {
-  this.dependency = dependency;
-};
+var DI = function (dependency) {
+  this.dependency = dependency
+}
 
 // Should return new function with resolved dependencies
-DI.prototype.inject = function(func) {
-  let deps = /\(([\w,_\s]+)\)/g.exec(func.toString()),
-    self = this,
-    resolved;
+DI.prototype.inject = function (func) {
+  /\(([\w,_\s]+)\)/g.exec(func.toString())
+  let self = this
+  let resolved
 
-    if (args !== null) {
-      args = args[1];
-      resolved = args.split(',')
-        .map(item => self.dependency[item.trim()]);
-    } else {
-      resolved = [];
-    }
-  return function() {
-    return func(...resolved);
-  };
-};
+  if (arguments !== null) {
+    let args = arguments[1]
+    resolved = args.split(',')
+      .map(item => self.dependency[item.trim()])
+  } else {
+    resolved = []
+  }
+  return function () {
+    return func(...resolved)
+  }
+}
 
-const assert = require('assert');
+const assert = require('assert')
 
-describe('DependencyInjector', function() {
-  it('should resolve the injected methods', function() {
+describe('DependencyInjector', function () {
+  it('should resolve the injected methods', function () {
     var deps = {
-      'dep1': function() {
-        return 'this is dep1';
+      'dep1': function () {
+        return 'this is dep1'
       },
-      'dep2': function() {
-        return 'this is dep2';
+      'dep2': function () {
+        return 'this is dep2'
       },
-      'dep3': function() {
-        return 'this is dep3';
+      'dep3': function () {
+        return 'this is dep3'
       },
-      'dep4': function() {
-        return 'this is dep4';
+      'dep4': function () {
+        return 'this is dep4'
       }
-    };
+    }
 
-    var di = new DI(deps);
+    var di = new DI(deps)
 
-    var myFunc = di.inject(function(dep3, dep1, dep2) {
-      return [dep1(), dep2(), dep3()].join(' -> ');
-    });
-    assert.equal(myFunc(), 'this is dep1 -> this is dep2 -> this is dep3');
+    var myFunc = di.inject(function (dep3, dep1, dep2) {
+      return [dep1(), dep2(), dep3()].join(' -> ')
+    })
+    assert.equal(myFunc(), 'this is dep1 -> this is dep2 -> this is dep3')
+  })
 
-  });
-
-  it('should resolve the case when no parameters sent', function() {
-    var di = new DI({});
-    var myFunc = di.inject(function() {
-      return "hello world";
-    });
-    assert.equal(myFunc(), 'hello world');
-
-  });
-});
+  it('should resolve the case when no parameters sent', function () {
+    var di = new DI({})
+    var myFunc = di.inject(function () {
+      return 'hello world'
+    })
+    assert.equal(myFunc(), 'hello world')
+  })
+})
