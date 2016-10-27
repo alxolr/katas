65bf91e49b77d2bd32c774c240655c750d10e8f6
Alexandru Olaru <aolaru@pentalog.fr>
2016-10-27 17:19:59 +0300

    Start working on robot path

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 #Problems started but not finished
 - `/test/robot-path.test.js/`
-- `/random-triplets-secret.test.js`
--- test/random-triplets-secret.test.js
+++ test/random-triplets-secret.test.js
@@ -1,104 +1,14 @@
 ;(() => {
   'use strict'
 
-  function recoverSecret (triplets) {
-    let rules = {}
-
-    triplets.forEach((triplet) => {
-      let [f, s, t] = triplet
-
-      if (!rules.hasOwnProperty(f)) {
-        rules[f] = {
-          before: new Set(),
-          after: new Set()
-        }
-        rules[f].before.add(s)
-        rules[f].before.add(t)
-      } else {
-        rules[f].before.add(s)
-        rules[f].before.add(t)
-      }
-
-      if (!rules.hasOwnProperty(s)) {
-        rules[s] = {
-          before: new Set(),
-          after: new Set()
-        }
-        rules[s].before.add(t)
-        rules[s].after.add(f)
-      } else {
-        rules[s].before.add(t)
-        rules[s].after.add(f)
-      }
-
-      if (!rules.hasOwnProperty(t)) {
-        rules[t] = {
-          before: new Set(),
-          after: new Set()
-        }
-        rules[t].after.add(f)
-        rules[t].after.add(s)
-      } else {
-        rules[t].after.add(f)
-        rules[t].after.add(s)
-      }
-    })
-
-    for (let letter in rules) {
-      rules[letter].before = Array.from(rules[letter].before)
-      rules[letter].after = Array.from(rules[letter].after)
-    }
-    console.log(rules)
-
-    return generateWord(rules)
-  }
-
-  function generateWord (rules) {
-    let word = []
-    let reverse = []
-    let before = ''
-    let after = ''
-    for (let letter in rules) {
-      if (rules[letter].after.length === 0) {
-        word.push(letter)
-        after = letter
+  const recoverSecret = function (triplets) {
+    for (var [first] of triplets) {
+      if (triplets.every(tuple => tuple.indexOf(first) <= 0)) {
+        triplets.filter(([item]) => item === first).forEach(tuple => tuple.shift())
+        return first + recoverSecret(triplets.filter(tuple => tuple.length > 0))
       }
     }
-    let hasEntered = true
-    while (hasEntered) {
-      for (let letter in rules) {
-        if (rules[letter].after.length === 1 && rules[letter].after[0] === after) {
-          word.push(letter)
-          after = letter
-          hasEntered = true
-          continue
-        }
-        hasEntered = false
-      }
-    }
-
-    for (let letter in rules) {
-      if (rules[letter].before.length === 0) {
-        reverse.push(letter)
-        before = letter
-      }
-    }
-    hasEntered = true
-    while (hasEntered) {
-      for (let letter in rules) {
-        console.log(letter, before)
-        if ((rules[letter].before.length === 1) && (rules[letter].before[0] === before)) {
-          reverse.push(letter)
-          before = letter
-          console.log(before)
-          hasEntered = true
-          continue
-        }
-        hasEntered = false
-      }
-    }
-
-    return word + reverse.reverse()
+    return ''
   }
 
   let triplets = [
